545b047fd720de4fa405f392642f15cf1bc33c38
limafabricioo <145229500+limafabricioo@users.noreply.github.com>
2024-04-07 18:36:07 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from classes import Task
 
+from sqlite3
+
+
+#engine da sessão
+#engine = create_engine('sqite:///Tasks.db')
+#Session = sessionmaker(bind=engine)
+#session = Session()
+
+#Mapeando a classe para a tabela
+#metadata = MetaData
+#tarefas = table('tarefas', metadata, autoload_with=engine)
+#mapper(Taks, tarefas)
+
+#Entradas do usuário
+
+
+#nome_tarefa = input("Digite o nome da tarefa: ")
+#nome_descricao = input("Digite a descrição desta tarefa: ")
+#print(data_hoje)
