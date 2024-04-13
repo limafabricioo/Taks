c1e2360d6320f5f9d818276000cacb201c130a80
limafabricioo <fabricioposdata@gmail.com>
2024-04-12 21:32:28 -0300

    Atualizações novas

---
+++ main.py
@@ -0,0 +1,31 @@
+#from classes import Task
+
+#from sqlite3
+
+#Conectando ao banco de daos
+#bd_Task = sqlite3.connect('Task.bd')
+
+#Cursor para executar comando SQL
+#cursor_local = bd_Task.cursor()
+
+#cursor_local.execute('SELECT * FROM tarefas')
+#tarefas_local = cursor_local.fetchall()
+
+#for
+
+#engine da sessão
+#engine = create_engine('sqite:///Tasks.db')
+#Session = sessionmaker(bind=engine)
+#session = Session()
+
+#Mapeando a classe para a tabela
+#metadata = MetaData
+#tarefas = table('tarefas', metadata, autoload_with=engine)
+#mapper(Taks, tarefas)
+
+#Entradas do usuário
+
+
+#nome_tarefa = input("Digite o nome da tarefa: ")
+#nome_descricao = input("Digite a descrição desta tarefa: ")
+#print(data_hoje)
